32a83e4fc0e787f9c641f3e6029934d99d9e6ce7
Aldo Rafael Matus Angulo <Aldo.Matus@corp.nearshoretc.com>
2019-07-19 15:29:05 -0500

    Added and styled first draft of calendar header

--- mte-calendar.js
+++ mte-calendar.js
@@ -1,4 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/iron-icons/iron-icons.js';
 
 /**
  * `mte-calendar`
@@ -14,18 +16,105 @@ class MteCalendar extends PolymerElement {
       <style>
         :host {
           display: block;
+          --calendar-header-bkgnd: #FF675C;
+          --calendar-header-forecolor: #fff8f7;
         }
+
+        .header {
+          display: flex;
+          flex-direction: row;
+          background-color: var(--calendar-header-bkgnd);
+          border-top-left-radius: 3px;
+          border-top-right-radius: 3px;
+          height: 30px;
+          padding-top: 5px;
+          padding-bottom: 5px;
+        }
+
+        .header > * {
+          color: var(--calendar-header-forecolor);
+        }
+        
+        .title {
+          min-width: 60%;
+          font-weight: bold;
+          font-size: 18px;
+          text-align: center;
+          margin-top: 1.5%;
+        }
+
+        #previous,#next {
+          min-width: 20%;
+          margin:0;
+        }
+
+        .header paper-button iron-icon {
+          min-width: 100%;
+        }
+
+        @media(min-width: 425px)
+        {
+          .header {
+            height: 60px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+          }
+
+          .title {
+            min-width: 80%;
+            font-size: 24px;
+            margin-top: 3%;
+          }
+
+          #previous,#next {
+            min-width: 10%;
+            margin:0;
+          }
+
+        }
+
       </style>
-      <h2>Hello [[prop1]]!</h2>
+      
+      <div class="calendar">
+        <div class="header">
+          <paper-button id="previous">
+            <iron-icon icon="chevron-left"></iron-icon>
+          </paper-button>
+          
+          <div class="title">{{header_title}}</div>
+
+          <paper-button id="next">
+            <iron-icon icon="chevron-right"></iron-icon>
+          </paper-button>
+          
+        </div>
+      </div>
     `;
   }
+
   static get properties() {
     return {
-      prop1: {
-        type: String,
-        value: 'mte-calendar',
+      strfiedMonths: {
+        type: Array,
+        value: [
+          'January', 'February', 'March', 'April', 'May', 'June',
+          'July', 'August', 'September', 'October', 'November', 'December'
+        ]
       },
+      the_date: {
+        type: Object,
+        value: new Date(),
+      },
+      header_title: {
+        type: String,
+        computed: 'getHeaderTitle(the_date)'
+      }
+
     };
+  }
+
+  getHeaderTitle(date) {
+    return this.strfiedMonths[date.getMonth()] + ' '+ date.getFullYear();
   }
 }
 
